4ea7e8d5bb9a6fd7a0ef18a8fd097d56223a0e11
nahco314 <nahco3_ta@yahoo.co.jp>
2025-04-28 19:24:06 +0900

    feat: remove no-daemon option from format arguments

--- src/cli/format.rs
+++ src/cli/format.rs
@@ -18,52 +18,9 @@ use std::{fs, io};
 pub struct FormatArgs {
     /// Path to format
     pub path: PathBuf,
-    #[clap(long)]
-    pub no_daemon: bool,
-}
-
-fn format_with_no_daemon(args: FormatArgs, global_options: GlobalOptions) -> Result<()> {
-    let (config, cache_dir) =
-        load_config_and_cache(&global_options.config_file, &global_options.cache_dir)?;
-
-    let target_path = args.path.canonicalize()?;
-
-    let file = fs::File::open(&args.path)?;
-    let mut buf_reader = io::BufReader::new(file);
-    let mut content = String::new();
-    buf_reader.read_to_string(&mut content)?;
-
-    let rule = config
-        .find_matched_rule(&target_path, false)
-        .context("No rule matched")?;
-
-    debug_long!("run rule: {:?}", rule);
-
-    let res = run(
-        &rule.some_cmd,
-        json!({
-            "wasm-current-dir":  to_wasm_path(&current_dir()?)?,
-            "os-current-dir": normalize_path(&current_dir()?)?,
-            "wasm-target": to_wasm_path(&target_path)?,
-            "os-target":  normalize_path(&target_path)?,
-            "raw-target": args.path,
-            "target-content": content,
-        }),
-        &cache_dir,
-        !global_options.no_cache,
-    )?;
-
-    debug_long!("{:?}", res);
-    info!("Success to format");
-
-    Ok(())
 }
 
 pub fn format_execute_with_args(args: FormatArgs, global_options: GlobalOptions) -> Result<()> {
-    if args.no_daemon {
-        return format_with_no_daemon(args, global_options);
-    }
-
     let (_, socket_dir) =
         load_config_and_socket(&global_options.config_file, &global_options.socket_dir)?;
 
