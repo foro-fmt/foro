c47562d4ae5cc1768d2aa1dd1ae1409e7a88f2f9
nahco314 <nahco3_ta@yahoo.co.jp>
2025-06-11 10:21:41 +0900

    refactor(cli): remove `bulk_format` command and integrate functionality into `format` command

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use clap_verbosity_flag::InfoLevel;
 use std::path::PathBuf;
 
-mod bulk_format;
 mod cache;
 mod config;
 mod daemon;
@@ -12,7 +11,6 @@ mod internal;
 
 use format::*;
 
-use crate::cli::bulk_format::{bulk_format_execute_with_args, BulkFormatArgs};
 use crate::cli::cache::{cache_execute_with_args, CacheArgs};
 use crate::cli::config::{config_execute_with_args, ConfigArgs};
 use crate::cli::daemon::{daemon_execute_with_args, DaemonArgs};
@@ -27,7 +25,6 @@ pub enum SubCommands {
     Config(ConfigArgs),
     Daemon(DaemonArgs),
     Format(FormatArgs),
-    BulkFormat(BulkFormatArgs),
     #[clap(hide = true)]
     Internal(InternalArgs),
 }
@@ -99,7 +96,6 @@ pub fn execute_with_args(args: Command) -> Result<()> {
         SubCommands::Config(s_args) => config_execute_with_args(s_args, global_options),
         SubCommands::Daemon(s_args) => daemon_execute_with_args(s_args, global_options),
         SubCommands::Format(s_args) => format_execute_with_args(s_args, global_options),
-        SubCommands::BulkFormat(s_args) => bulk_format_execute_with_args(s_args, global_options),
         SubCommands::Internal(s_args) => internal_execute_with_args(s_args, global_options),
     }?;
 
--- src/cli/format.rs
+++ src/cli/format.rs
@@ -1,15 +1,19 @@
 use crate::cli::GlobalOptions;
 use crate::config::load_config_and_socket;
 use crate::daemon::client::{ensure_daemon_running, run_command as daemon_run_command};
-use crate::daemon::interface::{DaemonCommands, DaemonFormatArgs, DaemonSocketPath};
+use crate::daemon::interface::{DaemonBulkFormatArgs, DaemonCommands, DaemonFormatArgs, DaemonSocketPath};
 use anyhow::Result;
 use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 pub struct FormatArgs {
-    /// Path to format
-    pub path: PathBuf,
+    /// Paths to format
+    #[clap(default_value = ".")]
+    pub paths: Vec<PathBuf>,
+    /// Number of threads to use
+    #[clap(short, long, default_value = "0")]
+    pub threads: usize,
 }
 
 pub fn format_execute_with_args(args: FormatArgs, global_options: GlobalOptions) -> Result<()> {
@@ -22,12 +26,32 @@ pub fn format_execute_with_args(args: FormatArgs, global_options: GlobalOptions)
 
     ensure_daemon_running(&socket, &global_options)?;
 
-    daemon_run_command(
-        DaemonCommands::Format(DaemonFormatArgs { path: args.path }),
-        global_options,
-        &socket,
-        false,
-    )?;
+    // If only one path is given and it's a file, use Format command
+    if args.paths.len() == 1 && args.paths[0].is_file() {
+        daemon_run_command(
+            DaemonCommands::Format(DaemonFormatArgs { path: args.paths[0].clone() }),
+            global_options,
+            &socket,
+            false,
+        )?;
+    } else {
+        // Otherwise, use BulkFormat command
+        let threads = if args.threads == 0 {
+            num_cpus::get()
+        } else {
+            args.threads
+        };
+
+        daemon_run_command(
+            DaemonCommands::BulkFormat(DaemonBulkFormatArgs {
+                paths: args.paths,
+                threads,
+            }),
+            global_options,
+            &socket,
+            true,
+        )?;
+    }
 
     Ok(())
 }
